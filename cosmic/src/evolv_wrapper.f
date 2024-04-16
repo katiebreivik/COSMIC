12a79e417ebb4c68b9042be52f50bddca6d14992
katiebreivik <kbreivik@flatironinstitute.org>
2024-04-16 10:56:46 -0400

    update wrapper for bh3

--- cosmic/src/evolv_wrapper.f
+++ cosmic/src/evolv_wrapper.f
@@ -1,5 +1,5 @@
       
-      SUBROUTINE evolv2_global(z,zpars,acclim,alphain,qHG,qGB,kick_in)
+      SUBROUTINE evolv2_global(z,zpars,acc,alph,qGB,qCHeB,qAGB,kick_in)
       
       implicit none
       INCLUDE 'const_bse.h'
@@ -10,9 +10,9 @@
       REAL*8 epoch(2),tms(2),tphys,tphysf,dtp,aj
       REAL*8 rad(2),lum(2),ospin(2),kick_info_out(2,17)
       REAL*8 massc(2),radc(2),menv(2),renv(2),bhspin(2)
-      REAL*8 tb,ecc,yearsc,bkick(20),alphain(2)
+      REAL*8 tb,ecc,yearsc,bkick(20),alph(2)
       REAL*8 B_0(2),bacc(2),tacc(2),xip,xihold
-      REAL*8 acclim(2),qHG,qGB
+      REAL*8 acc(2),qGB,qCHeB,qAGB
       REAL*8 kick_in(2,5)
 
       PARAMETER(yearsc=3.1557d+07)
@@ -73,15 +73,15 @@
       lambdaf = 0.0
       gamma = -2.0
       polar_kick_angle = 90.0
-      acc_lim = acclim
+      acc = acc
 *not sure why this idum is here...
-      alpha1(1) = alphain(1)
-      alpha1(2) = alphain(2)
+      alpha1(1) = alph(1)
+      alpha1(2) = alph(2)
 
-      idum = 50	
-      do j=1,2	
-         do i=1,12	
-            kick_info(j,i) = 0.d0	
+      idum = 50
+      do j=1,2
+         do i=1,12
+            kick_info(j,i) = 0.d0
          enddo	
          do i=1,5	
             natal_kick_array(j,i) = kick_in(j,i)	
@@ -93,8 +93,10 @@
          fprimc_array(j) = 2.0/21.0
       enddo      
 *     Fortran indexes from 1, HG is kstar=2, GB is kstar=3
-      qcrit_array(3) = qHG	
       qcrit_array(4) = qGB
+      qcrit_array(5) = qCHeB      
+      qcrit_array(6) = qAGB	
+	
 
       CALL zcnsts(z,zpars)
       CALL instar
